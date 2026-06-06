e049d82a4e9c50d7a5fa453a87f338598ea23f02
Konstantin Voykov <koykov0@gmail.com>
2026-06-07 00:01:47 +0300

    dtls: versions

--- version.go
+++ version.go
@@ -17,15 +17,15 @@ func (mv Version) Hi() uint8 {
 func (mv Version) Short() string {
 	lo, hi := byte(mv), byte(mv>>8)
 	switch {
-	case hi == 3 && lo == 0:
+	case hi == 0x03 && lo == 0x00:
 		return "30"
-	case hi == 3 && lo == 1:
+	case hi == 0x03 && lo == 0x01:
 		return "10"
-	case hi == 3 && lo == 2:
+	case hi == 0x03 && lo == 0x02:
 		return "11"
-	case hi == 3 && lo == 3:
+	case hi == 0x03 && lo == 0x03:
 		return "12"
-	case hi == 3 && lo == 4:
+	case hi == 0x03 && lo == 0x04:
 		return "13"
 	default:
 		return "00"
@@ -35,18 +35,22 @@ func (mv Version) Short() string {
 func (mv Version) String() string {
 	lo, hi := byte(mv), byte(mv>>8)
 	switch {
-	case hi == 3 && lo == 0:
+	case hi == 0x03 && lo == 0x00:
 		return "SSL3.0"
-	case hi == 3 && lo == 1:
+	case hi == 0x03 && lo == 0x01:
 		return "TLS1.0"
-	case hi == 3 && lo == 2:
+	case hi == 0x03 && lo == 0x02:
 		return "TLS1.1"
-	case hi == 3 && lo == 3:
+	case hi == 0x03 && lo == 0x03:
 		return "TLS1.2"
-	case hi == 3 && lo == 4:
+	case hi == 0x03 && lo == 0x04:
 		return "TLS1.3"
-	case hi == 254 && lo == 253:
+	case hi == 0xfe && lo == 0xff:
+		return "DTLS1.0"
+	case hi == 0xfe && lo == 0xfd:
 		return "DTLS1.2"
+	case hi == 0xfe && lo == 0xfc:
+		return "DTLS1.3"
 	default:
 		return "UNK"
 	}
